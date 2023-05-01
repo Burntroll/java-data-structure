f293bb50ab11d3624f92710b48ddb2bf201e5a02
Francisco Silva <fco.silva.dev@gmail.com>
2023-05-01 22:09:37 +0300

    Add queue

---
+++ 11 - queue/queue/src/main/java/com/burntroll/Main.java
@@ -0,0 +1,34 @@
+package com.burntroll;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue<Carro> queueCarros = new LinkedList<>();
+
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Chevrolet"));
+        queueCarros.add(new Carro("Fiat"));
+
+        System.out.println(queueCarros.add(new Carro("Hyundai")));
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.offer(new Carro("Honda")));
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.peek());
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.poll());
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.isEmpty());
+
+        System.out.println(queueCarros.size());
+
+        System.out.println(queueCarros);
+
+    }
+}
